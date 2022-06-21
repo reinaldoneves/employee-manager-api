f0cccb6225c70c23e2fb1383eb12724c1eb036c5
reinaldoneves <reinaldo_neves@hotmail.com>
2022-06-21 14:26:55 -0300

    Service and repo impl

--- src/main/java/br/com/naldo/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/br/com/naldo/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,11 @@ package br.com.naldo.employeemanager.repo;
 import br.com.naldo.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+
 }
--- src/main/java/br/com/naldo/employeemanager/service/EmployeeService.java
+++ src/main/java/br/com/naldo/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import br.com.naldo.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,6 +21,22 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return repo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return repo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return repo.save(employee);
+    }
+
+    public void deleteEmployee(Long id){
+        repo.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return repo.findEmployeeById(id).orElseThrow();
     }
 
 }
