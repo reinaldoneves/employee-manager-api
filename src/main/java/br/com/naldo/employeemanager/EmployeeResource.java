a1a36aed66553b8897e5cd3b56a718a310c1de78
reinaldoneves <reinaldo_neves@hotmail.com>
2022-06-21 15:02:14 -0300

    Controller impl - CRUD

--- src/main/java/br/com/naldo/employeemanager/EmployeeResource.java
+++ src/main/java/br/com/naldo/employeemanager/EmployeeResource.java
@@ -5,10 +5,7 @@ import br.com.naldo.employeemanager.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +30,24 @@ public class EmployeeResource {
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
         Employee employee = service.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = service.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updatedEmployee = service.updateEmployee(employee);
+        return new ResponseEntity<>(updatedEmployee, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
+        service.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
